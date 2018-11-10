7934e107b252150a239e46e159877444eeda662e
Neema Valizadeh <nvalizad@umd.edu>
2018-11-10 16:03:04 -0500

    a few strings added in strings.xml two buttons and two edittexts in mainActivity.xml implemented on create and started on click functions for signup and login TODOS inserted for brian and steven

--- app/src/main/java/com/example/neema/storyboard/MainActivity.java
+++ app/src/main/java/com/example/neema/storyboard/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.neema.storyboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.view.View;
+import android.content.Intent;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    static EditText uname;
+    static EditText passwd;
+
 //update
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+         uname = findViewById(R.id.username_edittext);
+         passwd = findViewById(R.id.password_edittext);
+
+
+
+    }
+    public void signupAction(View v){
+        Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+        //TODO SignupActivity.java needs to be implemented
+        startActivity(intent);
+
+    }
+    public void loginAction(View v){
+        //TODO database communication
     }
 }
