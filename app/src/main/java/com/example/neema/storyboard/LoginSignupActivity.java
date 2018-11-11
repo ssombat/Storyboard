4caf859c136b37e14035742a1dd09d174340e083
brianquach123 <brianquach123@gmail.com>
2018-11-11 10:12:44 -0500

    setup for signup/login screen (BQ's layout)

--- app/src/main/java/com/example/neema/storyboard/LoginSignupActivity.java
+++ app/src/main/java/com/example/neema/storyboard/LoginSignupActivity.java
@@ -4,32 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
-import android.widget.EditText;
 import android.view.View;
 import android.content.Intent;
 
-
+import android.widget.EditText;
+import android.widget.Button;
 
 public class LoginSignupActivity extends AppCompatActivity {
-    static EditText uname;
-    static EditText passwd;
+
+    EditText username, password;
+    Button loginButton, signupButton;
 
     //update
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signup);
+        //uname = findViewById(R.id.username_edittext);
+        //passwd = findViewById(R.id.password_edittext);
 
-         uname = findViewById(R.id.username_edittext);
-         passwd = findViewById(R.id.password_edittext);
-
+        loginButton = findViewById(R.id.loginSubmit);
+        signupButton = findViewById(R.id.signupSubmit);
 
 
     }
+
+    public void signup(View v) {
+
+    }
+
+    public void login(View v) {
+
+    }
+
     public void signupAction(View v){
-        Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+        //Intent intent = new Intent(MainActivity.this, SignupActivity.class);
         //TODO SignupActivity.java needs to be implemented
-        startActivity(intent);
+        //startActivity(intent);
 
     }
     public void loginAction(View v){
