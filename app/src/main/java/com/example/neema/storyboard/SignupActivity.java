1e7c8d9fed7f894308ba803190b055a67cc9a25a
Steven Sombat <sombatsteven@gmail.com>
2018-11-11 18:22:16 -0500

    Signup function done.

--- app/src/main/java/com/example/neema/storyboard/SignupActivity.java
+++ app/src/main/java/com/example/neema/storyboard/SignupActivity.java
@@ -1,5 +1,6 @@
 package com.example.neema.storyboard;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,11 +8,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignupActivity extends AppCompatActivity {
 
     Button signupButton;
     EditText username, fullName, email, password, passwordConfirm;
     String usernameString, fullNameString, emailString, passwordString, passwordConfirmString;
+
+    private FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference mRefUserTable = mFirebaseDatabase.getReference("UserTable");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,9 +49,9 @@ public class SignupActivity extends AppCompatActivity {
         passwordConfirmString = passwordConfirm.getText().toString();
 
         if (isInfoEntered(usernameString, fullNameString, emailString, passwordString, passwordConfirmString)) {
-            if (passwordString == passwordConfirmString) {
-                User newUser = new User(usernameString, fullNameString, emailString, passwordString);
+            if (passwordString.equals(passwordConfirmString)) {
                 //TODO: upload new user to firebase
+                registerRequest(usernameString, fullNameString, emailString, passwordString);
             }
             else {
                 Toast.makeText(getApplicationContext(),
@@ -51,5 +64,23 @@ public class SignupActivity extends AppCompatActivity {
                     getString(R.string.error_missing_field),
                     Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void registerRequest(final String usernameString, final String fullNameString, final String emailString, String passwordString) {
+        mFirebaseAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // The user has a unique id that we will use as a primary key
+                    String userId = mFirebaseAuth.getCurrentUser().getUid();
+                    User newUser = new User(usernameString, fullNameString, emailString, userId);
+                    mRefUserTable.child(userId).setValue(newUser);
+                    mFirebaseAuth.getCurrentUser().sendEmailVerification();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Registration Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
