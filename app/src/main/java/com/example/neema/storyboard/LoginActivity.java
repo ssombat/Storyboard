e6ce914b9d187fe14e0be90f705ad41bc416e250
Steven Sombat <sombatsteven@gmail.com>
2018-11-12 12:40:22 -0500

    Login function works now.

--- app/src/main/java/com/example/neema/storyboard/LoginActivity.java
+++ app/src/main/java/com/example/neema/storyboard/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.neema.storyboard;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,10 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
 
     Button loginButton, forgotPasswordButton;
     EditText emailInput, passwordInput;
+    private FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +31,27 @@ public class LoginActivity extends AppCompatActivity {
         passwordInput = findViewById(R.id.passwordLoginInput);
     }
     protected void loginPressed(View v) {
-        if (verifyCredentials(emailInput.getText().toString(), passwordInput.getText().toString())) {
-            startActivity(new Intent(LoginActivity.this, HomepageActivity.class));
-        }
-        else {
-            Toast.makeText(getApplicationContext(),
-                    getString(R.string.invalid_login_credentials),
-                    Toast.LENGTH_LONG).show();
-        }
+        loginRequest(emailInput.getText().toString(), passwordInput.getText().toString());
     }
     protected void forgotPasswordPressed(View v) {
         startActivity(new Intent(LoginActivity.this, ForgotPasswordActivity.class));
     }
 
-    boolean verifyCredentials(String email, String password) {
-        //TODO: Verify credentials with firebase here
-        return false;
+    private void loginRequest(String email, String password){
+        mFirebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // TODO: Need to pass in the uid of the login user to the intent
+                    String userUid = mFirebaseAuth.getCurrentUser().getUid();
+                    startActivity(new Intent(LoginActivity.this, HomepageActivity.class));
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),
+                            getString(R.string.invalid_login_credentials),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
