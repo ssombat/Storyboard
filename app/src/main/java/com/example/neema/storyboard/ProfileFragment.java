42792e455e1009d8a8ec1480e8024955097f701e
waynema <wma@umd.edu>
2018-11-17 22:26:46 -0500

    some skeleton of the profile fragment and the flashcards fragments

--- app/src/main/java/com/example/neema/storyboard/ProfileFragment.java
+++ app/src/main/java/com/example/neema/storyboard/ProfileFragment.java
@@ -5,14 +5,29 @@ import android.view.View;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.content.Intent;
 
 public class ProfileFragment extends Fragment {
-    //TODO ADD THINGS NEEDED FOR THE DATABASE SUCH AS NAME
-
+    ImageView profilePic;
+    EditText bio;
+    Button button;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         //TODO fill in the stuff for the layout for the profile fragment
-        return inflater.inflate(R.layout.profile,container,false);
+        View view = inflater.inflate(R.layout.profile,container,false);
+        profilePic = view.findViewById(R.id.profilePicture);
+        bio = view.findViewById(R.id.bio);
+        button = view.findViewById(R.id.settings);
+        return view;
+
+    }
+    protected void settingFrag(View v) {
+        Intent intent = new Intent(getActivity(),ProfileSetting.class);
+        startActivity(intent);
     }
 }
