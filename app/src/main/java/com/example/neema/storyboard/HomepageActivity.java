4b8528d36dd192e9962746e823ec38e6f844f4ce
brianquach123 <brianquach123@gmail.com>
2018-11-18 00:36:47 -0500

    refactor profile initialization logic, fix crashing issue with fragment buttons

--- app/src/main/java/com/example/neema/storyboard/HomepageActivity.java
+++ app/src/main/java/com/example/neema/storyboard/HomepageActivity.java
@@ -2,17 +2,24 @@ package com.example.neema.storyboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomepageActivity extends AppCompatActivity {
 
     private FlashCardsFragment flashcards;
     private ProfileFragment profile;
+    private Button settingsButton, newPostButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
         profile = (ProfileFragment) getFragmentManager().findFragmentById(R.id.profile);
         //flashcards = (FlashCardsFragment) getFragmentManager().findFragmentById(R.id.cardList);
+
+    }
+
+    protected void newPostButtonPressed() {
 
     }
 }
--- app/src/main/java/com/example/neema/storyboard/ProfileFragment.java
+++ app/src/main/java/com/example/neema/storyboard/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.neema.storyboard;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.view.View;
 import android.os.Bundle;
@@ -10,24 +11,50 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class ProfileFragment extends Fragment {
     ImageView profilePic;
     EditText bio;
-    Button button;
+    Button settingsButton, composeButton;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         //TODO fill in the stuff for the layout for the profile fragment
-        View view = inflater.inflate(R.layout.profile,container,false);
+        return inflater.inflate(R.layout.profile,container,false);
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         profilePic = view.findViewById(R.id.profilePicture);
         bio = view.findViewById(R.id.bio);
-        button = view.findViewById(R.id.settings);
-        return view;
+        settingsButton = view.findViewById(R.id.settingsSubmit);
+        composeButton = view.findViewById(R.id.composeSubmit);
 
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "settings in profile fragment hit", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        composeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newPostButtonPressed();
+            }
+        });
     }
+
     protected void settingFrag(View v) {
         Intent intent = new Intent(getActivity(),ProfileSetting.class);
         startActivity(intent);
+    }
+
+    protected void newPostButtonPressed() {
+        Toast.makeText(getActivity(), "new post requested", Toast.LENGTH_SHORT).show();
+        
     }
 }
