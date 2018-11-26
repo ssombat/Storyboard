24d5e6455a4a55b3447edf76b2a1df2d89c68e90
brianquach123 <brianquach123@gmail.com>
2018-11-25 21:20:34 -0500

    users can now change prompt and freewrite titles via fab

--- app/src/main/java/com/example/neema/storyboard/FreeWriteActivity.java
+++ app/src/main/java/com/example/neema/storyboard/FreeWriteActivity.java
@@ -1,10 +1,13 @@
 package com.example.neema.storyboard;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -17,8 +20,13 @@ public class FreeWriteActivity extends AppCompatActivity {
 
     EditText draftText;
     TextView visibilityText;
+    TextView titleText;
     Switch privacySwitch;
     boolean isPrivate;
+
+    Toolbar toolbar;
+    //For editing title
+    String titlePlaceholderText = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +35,24 @@ public class FreeWriteActivity extends AppCompatActivity {
 
         draftText = findViewById(R.id.draftText);
         visibilityText = findViewById(R.id.visibilityText);
+        titleText = findViewById(R.id.toolbarTitle);
         privacySwitch = findViewById(R.id.privacySwitch);
         isPrivate = privacySwitch.isChecked();
         FloatingActionButton saveButton = (FloatingActionButton) findViewById(R.id.saveButton);
         FloatingActionButton favButton = (FloatingActionButton) findViewById(R.id.favButton);
         FloatingActionButton titleButton = (FloatingActionButton) findViewById(R.id.titleButton);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         privacySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                 if (isChecked) {
-                    visibilityText.setText("Visibility: Private");
+                    visibilityText.setText("Private");
                 }
                 else {
-                    visibilityText.setText("Visibility: Public");
+                    visibilityText.setText("Public");
                 }
                 isPrivate = isChecked;
             }
@@ -73,6 +82,31 @@ public class FreeWriteActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),
                         "Title edit button pressed",
                         Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder inputDialog = new AlertDialog.Builder(FreeWriteActivity.this);
+                inputDialog.setTitle("Enter New Title:");
+
+                final EditText input = new EditText(FreeWriteActivity.this);
+                input.setPadding(80,25,80,0);
+                input.setBackground(null);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+
+                inputDialog.setView(input);
+
+                inputDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                inputDialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        titlePlaceholderText = input.getText().toString();
+                        titleText.setText(titlePlaceholderText);
+                    }
+                });
+                inputDialog.show();
             }
         });
     }
--- app/src/main/java/com/example/neema/storyboard/PromptActivity.java
+++ app/src/main/java/com/example/neema/storyboard/PromptActivity.java
@@ -1,10 +1,13 @@
 package com.example.neema.storyboard;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -16,8 +19,12 @@ public class PromptActivity extends AppCompatActivity {
 
     EditText draftText;
     TextView visibilityText;
+    TextView titleText;
     Switch privacySwitch;
     boolean isPrivate;
+
+    //For editing title
+    String titlePlaceholderText = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,7 @@ public class PromptActivity extends AppCompatActivity {
 
         draftText = findViewById(R.id.draftText);
         visibilityText = findViewById(R.id.visibilityText);
+        titleText = findViewById(R.id.toolbarTitle);
         privacySwitch = findViewById(R.id.privacySwitch);
         isPrivate = privacySwitch.isChecked();
         FloatingActionButton saveButton = (FloatingActionButton) findViewById(R.id.saveButton);
@@ -39,10 +47,10 @@ public class PromptActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                 if (isChecked) {
-                    visibilityText.setText("Visibility: Private");
+                    visibilityText.setText("Private");
                 }
                 else {
-                    visibilityText.setText("Visibility: Public");
+                    visibilityText.setText("Public");
                 }
                 isPrivate = isChecked;
             }
@@ -71,6 +79,31 @@ public class PromptActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),
                         "Title edit button pressed",
                         Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder inputDialog = new AlertDialog.Builder(PromptActivity.this);
+                inputDialog.setTitle("Enter New Title:");
+
+                final EditText input = new EditText(PromptActivity.this);
+                input.setPadding(80, 25, 80, 0);
+                input.setBackground(null);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+
+                inputDialog.setView(input);
+
+                inputDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                inputDialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        titlePlaceholderText = input.getText().toString();
+                        titleText.setText(titlePlaceholderText);
+                    }
+                });
+                inputDialog.show();
             }
         });
     }
