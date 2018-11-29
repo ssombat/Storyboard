3aa4b72585b6918aab94ec2e8300d3a3057deb12
Steven Sombat <sombatsteven@gmail.com>
2018-11-29 18:39:34 -0500

    Side Menu Done

--- app/src/main/java/com/example/neema/storyboard/SideMenu.java
+++ app/src/main/java/com/example/neema/storyboard/SideMenu.java
@@ -1,9 +1,12 @@
 package com.example.neema.storyboard;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,9 +16,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class SideMenu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    String currentUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference mRef = mFirebaseDatabase.getReference("UserTable").child(currentUser);
+    TextView username;
+    TextView name;
+    TextView email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +45,7 @@ public class SideMenu extends AppCompatActivity
         setContentView(R.layout.activity_side_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        setMenuHeader();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -73,28 +95,44 @@ public class SideMenu extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.community_cards) {
             // Handle the camera action
-            Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
-            startActivity(intent);
-        }
-        else if (id == R.id.nav_gallery) {
-            startActivity(new Intent(getApplicationContext(), WeeklyChallenge.class));
-
-        }
-        else if (id == R.id.nav_slideshow) {
             startActivity(new Intent(getApplicationContext(), CommunityActivity.class));
         }
-        else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        else if (id == R.id.weekly_challenge) {
+            startActivity(new Intent(getApplicationContext(), WeeklyChallenge.class));
+        }
+        else if (id == R.id.side_setting) {
+            startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void setMenuHeader () {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        //LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = navigationView.inflateHeaderView(R.layout.nav_header_side_menu);
+
+        username = view.findViewById(R.id.header_user);
+        name = view.findViewById(R.id.header_name);
+        email = view.findViewById(R.id.header_email);
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                username.setText((String) dataSnapshot.child("username").getValue());
+                name.setText((String) dataSnapshot.child("fullname").getValue());
+                email.setText((String) dataSnapshot.child("email").getValue());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
