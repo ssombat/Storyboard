15f92664a22d238619d1bcc0921600d7ebe160ad
waynema <wma@umd.edu>
2018-11-29 19:19:48 -0500

    took out the message

--- app/src/main/java/com/example/neema/storyboard/ProfileFragment.java
+++ app/src/main/java/com/example/neema/storyboard/ProfileFragment.java
@@ -59,10 +59,7 @@ public class ProfileFragment extends Fragment{
                         Toast.makeText(getActivity(), "prompt selected", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(getActivity(), PromptActivity.class));
                         return true;
-                    case R.id.settingsOption:
-                        Toast.makeText(getActivity(), "settings selected", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(getActivity(), SettingsActivity.class));
-                        return true;
+                    
                     default:
                         return false;
                 }
