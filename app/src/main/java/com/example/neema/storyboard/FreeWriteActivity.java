0198ad2fcd4a79e4251585f3f48c7d77a78ba661
Steven Sombat <sombatsteven@gmail.com>
2018-11-29 23:04:29 -0500

    Free Write Activity saves to the database.

--- app/src/main/java/com/example/neema/storyboard/FreeWriteActivity.java
+++ app/src/main/java/com/example/neema/storyboard/FreeWriteActivity.java
@@ -27,12 +27,14 @@ public class FreeWriteActivity extends AppCompatActivity {
     String currentUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
     FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
     DatabaseReference mRef = mDatabase.getReference("CardTable");
+    DatabaseReference mCommunityTable = mDatabase.getReference("CommunityTable");
     EditText draftText;
     TextView visibilityText;
     TextView titleText;
     Switch privacySwitch;
     private String CardID, Uid, privacyText;
     boolean isPublic;
+    boolean isNewCard;
 
     Toolbar toolbar;
     //For editing title
@@ -49,14 +51,14 @@ public class FreeWriteActivity extends AppCompatActivity {
         titleText = findViewById(R.id.toolbarTitle);
         privacySwitch = findViewById(R.id.privacySwitch);
         isPublic = privacySwitch.isChecked();
-
+        isNewCard = true;
         if (intent.getExtras()!= null){
             draftText.setText(intent.getStringExtra("Text"));
             titleText.setText(intent.getStringExtra("Title"));
             CardID = intent.getStringExtra("CardId");
             Uid = intent.getStringExtra("uid");
             isPublic = (intent.getBooleanExtra("Pub", false));
-
+            isNewCard = false;
             if (isPublic){
                 visibilityText.setText("Public");
                 isPublic = true;
@@ -69,6 +71,7 @@ public class FreeWriteActivity extends AppCompatActivity {
             }
 
         }
+
 
 //        FloatingActionButton saveButton = (FloatingActionButton) findViewById(R.id.saveButton);
         FloatingActionButton uploadButton = (FloatingActionButton) findViewById(R.id.uploadButton);
@@ -91,25 +94,6 @@ public class FreeWriteActivity extends AppCompatActivity {
             }
         });
 
-//        saveButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                /*
-//                TODO: DATA TO SUBMIT TO FIREBASE
-//                isPrivate
-//                titleText.getText().toString();
-//                draftText.getText().toString();*/
-//                String cardId = mRef.child(currentUser).child("Cards").push().getKey();
-//                Card card = new Card(CardType.FREEWRITE, currentUser, cardId, titleText.getText().toString(), draftText.getText().toString(), false);
-//
-//                mRef.child(currentUser).child("Cards").child(cardId).setValue(card);
-//
-
-//                Snackbar.make(view, "Saved", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -124,7 +108,38 @@ public class FreeWriteActivity extends AppCompatActivity {
                 inputDialog.setNegativeButton("Submit", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        //TODO: UPLOAD STORY CONTENTS TO THE COMMUNITY PAGE
+                        // add cards or edited old cards and places them in their respective database
+                        String cardId;
+                        if (!isPublic) {
+                            if (isNewCard) {
+                                cardId = mRef.child(currentUser).child("Cards").push().getKey();
+                                Card card = new Card(CardType.FREEWRITE, currentUser, cardId, titleText.getText().toString(), draftText.getText().toString(), false);
+                                mRef.child(currentUser).child("Cards").child(cardId).setValue(card);
+                            }
+                            else {
+                                cardId = CardID;
+                                Card card = new Card(CardType.FREEWRITE, currentUser, cardId, titleText.getText().toString(), draftText.getText().toString(), false);
+                                mRef.child(currentUser).child("Cards").child(cardId).setValue(card);
+                            }
+                        }
+                        else {
+                            if (isNewCard) {
+                                cardId = mRef.child(currentUser).child("Cards").push().getKey();
+                                Card card = new Card(CardType.FREEWRITE, currentUser, cardId, titleText.getText().toString(), draftText.getText().toString(), true);
+                                mRef.child(currentUser).child("Cards").child(cardId).setValue(card);
+
+                                //Adds to community table
+                                mCommunityTable.child(cardId).setValue(card);
+                            }
+                            else {
+                                cardId = CardID;
+                                Card card = new Card(CardType.FREEWRITE, currentUser, cardId, titleText.getText().toString(), draftText.getText().toString(), true);
+                                mRef.child(currentUser).child("Cards").child(cardId).setValue(card);
+
+                                //Adds to community table
+                                mCommunityTable.child(cardId).setValue(card);
+                            }
+                        }
                         //TODO: UPLOAD PROMPT BASED ON PRIVACY
                         Toast.makeText(getApplicationContext(),
                                 "Uploaded successfully!",
