f67afc74163137e4909f386e3b901820c0e69e7b
Steven Sombat <sombatsteven@gmail.com>
2018-11-29 19:36:28 -0500

    The profile now shows the current user's text.

--- app/src/main/java/com/example/neema/storyboard/ProfileFragment.java
+++ app/src/main/java/com/example/neema/storyboard/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.neema.storyboard;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,10 +18,21 @@ import android.content.Intent;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ProfileFragment extends Fragment{
-    TextView bio;
+    TextView bio, username;
     Button communityButton, composeButton;
     PopupMenu popupMenu;
+
+    String currentUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference mRef = mFirebaseDatabase.getReference("UserTable").child(currentUser);
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,9 +46,10 @@ public class ProfileFragment extends Fragment{
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         bio = view.findViewById(R.id.bio);
+        username = view.findViewById(R.id.usernameText);
         communityButton = view.findViewById(R.id.communitySubmit);
         composeButton = view.findViewById(R.id.composeSubmit);
-
+        setText();
         popupMenu = new PopupMenu(getActivity(), composeButton);
         MenuInflater inflater = popupMenu.getMenuInflater();
         inflater.inflate(R.menu.compose_actions, popupMenu.getMenu());
@@ -87,6 +100,21 @@ public class ProfileFragment extends Fragment{
 
     protected void newPostButtonPressed() {
         popupMenu.show();
+    }
+
+    public void setText() {
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                bio.setText((String) dataSnapshot.child("bio").getValue());
+                username.setText((String) dataSnapshot.child("username").getValue());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
